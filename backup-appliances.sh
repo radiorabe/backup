9ad76e50ddcac092545eccc4edcae5af7a4e4381
spameier <spameier@users.noreply.github.com>
2020-11-20 23:56:41 +0100

    (refactor) backup-appliances.sh

--- backup-appliances.sh
+++ backup-appliances.sh
@@ -1,62 +1,67 @@
 #!/bin/bash
 # backup files from appliances
+# TODO: DRY
 set -u
 . "$(dirname "$0")/backup-util.sh"
 
-APPLIANCE_LIST+=("***REMOVED***" "***REMOVED***" "***REMOVED***")
-BACKUP_DIRS="home/backup/ cf/conf/ etc/icecast*"
+GWS=("***REMOVED***" "***REMOVED***")
+GW_DIRS=("/home/backup/" "***REMOVED***/")
+STREAM="***REMOVED***"
+STREAM_DIR="***REMOVED****"
 
-# Main -------------------------------------------------------------------------
+main(){
+  log -i "Appliance backup starting"
+  mv ~/.ssh/known_hosts ~/.ssh/known_hosts.bkp
+  local errs_all=0
 
-# Source logging: https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-. $SCRIPT_DIR/logging.lib
+  # backup gateways
+  for gw in "${GWS[@]}"; do
+    local start; start=$(date +%s)
+    local errs=0
+    ssh-keyscan "$gw" >> ~/.ssh/known_hosts 2> /dev/null
+    for dir in "${GW_DIRS[@]}"; do
+      set -x
+      scp -rp -i "$SSH_KEY" "$SSH_USER@$gw:$dir" "$BACKUP_DST_DIR/$gw"
+      set +x
+      ret=$?
+      if [[ $ret -eq 0 ]]; then
+        log -s "Backup of $gw:$dir to $BACKUP_DST_DIR/$gw successful!"
+      else
+        log -e "Unknown error ($ret) occured when trying to backup $gw:$dir."
+        ((errs++))
+      fi
+    done
+    if [[ $errs -eq 0 ]]; then
+      if ! backup_success "$gw" "$start"; then
+        log -e "Could not send status for $gw to Zabbix"
+      fi
+    else
+      ((errs_vm_all++))
+      log -w "$gw had problems during the backup job"
+    fi
+  done
 
-mv ~/.ssh/known_hosts ~/.ssh/known_hosts.bkp
-
-logging -i "Appliance backup starting."
-
-errors_vm_all=0;
-for i in "${APPLIANCE_LIST[@]}"
-do
-  startTime="$(date +%s)";
-  vm_name="$i";
-  errors_vm=0;
-  ssh-keyscan $vm_name >> ~/.ssh/known_hosts 2>/dev/null
-  for j in $BACKUP_DIRS
-  do
-  syncdir=${vm_name}:/${j}
+  # backup stream
+  ssh-keyscan "$STREAM" >> ~/.ssh/known_hosts 2> /dev/null
   set -x
-  scp -rpi ${SSH_KEY} ${SSH_USER}@${syncdir} ${BACKUP_DST_DIR}/${i}
-  set +x
+  scp -rp -i "$SSH_KEY" "$SSH_USER@$STREAM:$STREAM_DIR" "$BACKUP_DST_DIR/$STREAM"
   ret=$?
-  if [ $ret -eq "0" ]
-  then
-    logging -s "Backup of $syncdir to ${BACKUP_DST_DIR}/${i} successfull!"
+  set +x
+  if [[ $ret -eq 0 ]]; then
+    log -s "Backup of $STREAM:$STREAM_DIR to $BACKUP_DST_DIR/$STREAM successful!"
+    if ! backup_success "$gw" "$start"; then
+      log -e "Could not send status for $gw to Zabbix"
+    fi
   else
-   logging -e "Unknown error ($ret) occured when trying to backup $syncdir."
-   let "errors_vm++";
+    log -e "Unknown error ($ret) occured when trying to backup $gw:$dir."
+    ((errs_all++))
   fi
-done
 
-if [ $errors_vm -eq 0 ];
-then
-  if ! backup_success $vm_name $startTime;
-  then
-      logging -e "Could not send statuses for ${zabbixHostName} to zabbix"
+  mv ~/.ssh/known_hosts.bkp ~/.ssh/known_hosts
+  log -i "Script finished; $errs_all appliances had problems during the backup job."
+  if [[ $errs_all -gt 0 ]]; then
+    exit 1
   fi
-else
-  let "errors_vm_all++";
-  logging -w "$vm_name had problems during the backup job."
-fi
-
-done
-
-mv ~/.ssh/known_hosts.bkp ~/.ssh/known_hosts
-logging -i "Script finished; $errors_vm_all appliances had problems during the backup job."
-
-if [ $errors_vm_all -gt 0 ];
-then
-  exit 1
-fi
+}
+main
 # vim: tabstop=2 shiftwidth=2 expandtab
