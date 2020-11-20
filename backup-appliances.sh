b934c85c9c0283deb462710b5ccde904d5413782
spameier <spameier@users.noreply.github.com>
2020-11-20 23:31:45 +0100

    (refactor) move backup_success and vars to util script

--- backup-appliances.sh
+++ backup-appliances.sh
@@ -1,87 +1,10 @@
-#!/bin/sh
-#
-#    This file is part of `Radio RaBe Backup process and automation scripts`
-#    Copyright (C) 2016 Radio Bern RaBe
-#    https://github.com/radiorabe/backup
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
-# Constants --------------------------------------------------------------------
-#
-PN="`basename "$0" .sh`"
-LOGFILE="/var/log/${PN}.log"  # logfile.
-BACKUP_DIRS="home/backup/ cf/conf/ etc/icecast*"
-SSH_USER="backup"
-SSH_KEY="/home/backup/.ssh/id_rsa"
-BACKUP_DST_DIR=/srv/backup/remote-backup
+#!/bin/bash
+# backup files from appliances
+set -u
+. "$(dirname "$0")/backup-util.sh"
+
 APPLIANCE_LIST+=("***REMOVED***" "***REMOVED***" "***REMOVED***")
-
-function backup_success()
-{
-
-
-startTime=$2;
-
-if [ -z $startTime ];
-then
-  logging -w "Backup start time was not set!"
-  return 1
-fi
-
-zabbixHostName=$1
-
-if [ -z $zabbixHostName ];
-then
-  logging -w "Could not recognize zabbix hostname!"
-  return 1
-fi
-
-timestamp=$(date +%s);
-duration=$(($timestamp - $startTime));
-Ret=0
-
-# Send the timestamp of the last successfull backup
-zabbix_sender --config /etc/zabbix/zabbix_agentd.conf \
-              --host "${zabbixHostName}" \
-              --key 'rabe.rabe-backup.run.success[]' \
-              --value "$timestamp" || Ret=$?
-
-# Send the duration of the last backup run in seconds
-zabbix_sender --config /etc/zabbix/zabbix_agentd.conf \
-              --host "${zabbixHostName}" \
-              --key 'rabe.rabe-backup.run.duration[]' \
-              --value "$duration" || Ret=$?
-
-return $Ret;
-}
-
-# trap keyboard interrupt (control-c)
-trap control_c SIGINT
-
-control_c()
-#
-# Description:  run if user hits control-c
-#
-# Parameter  :  none
-#
-# Output     :  logging
-#
-{
-logging -n "rabe-backup CTRL-C catched"
-exit 1
-}
+BACKUP_DIRS="home/backup/ cf/conf/ etc/icecast*"
 
 # Main -------------------------------------------------------------------------
 
@@ -136,3 +59,4 @@ if [ $errors_vm_all -gt 0 ];
 then
   exit 1
 fi
+# vim: tabstop=2 shiftwidth=2 expandtab
