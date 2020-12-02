fe5b67f6d3b3effb7b96e424b124b8a250a0426b
spameier <spameier@users.noreply.github.com>
2020-12-02 23:51:15 +0100

    (refactor) backup-userdata.sh

--- backup-userdata.sh
+++ backup-userdata.sh
@@ -1,187 +1,40 @@
-#!/bin/sh
-#
-#    This file is part of `Radio RaBe Backup process and automation scripts`
-#    Copyright (C) 2016 Radio Bern RaBe
-#    https://github.com/radiorabe/backup
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
-# Constants --------------------------------------------------------------------
-#
+#!/bin/bash
+# backup user shares
+. "$(dirname "$0")/backup-util.sh"
+
 BACKUP_SRC_HOST="***REMOVED***.***REMOVED***"
 BACKUP_SRC_DIRS=(
-    '***REMOVED***/'
-    '***REMOVED***/'
-    '***REMOVED***/'
-    '***REMOVED***/'
-    )
+    "***REMOVED***/"
+    "***REMOVED***/"
+    "***REMOVED***/"
+    "***REMOVED***/"
+)
 BACKUP_DST_DIRS=(
-    '***REMOVED***'
-    '***REMOVED***'
-    '***REMOVED***'
-    '***REMOVED***'
-    )
-SSH_USER="backup"
-SSH_KEY="/home/backup/.ssh/id_rsa"
-RSH_CMD="/usr/bin/ssh -i ${SSH_KEY} -l ${SSH_USER}"
-BACKUP_DST_DIR=/srv/backup/remote-backup
-RSYNC_OPTS="--archive --recursive --acls --devices --specials --delete --numeric-ids --timeout=120 --stats --human-readable --progress --inplace --one-file-system" 
-#RSYNC_OPTS="--verbose --archive --recursive --acls --devices --specials --delete --numeric-ids --timeout=120 --stats --human-readable --progress --inplace --one-file-system" 
+    "***REMOVED***"
+    "***REMOVED***"
+    "***REMOVED***"
+    "***REMOVED***"
+)
 
-
-# Functions --------------------------------------------------------------------
-
-function backup_success()
-{
-
-
-startTime=$1;
-
-if [ -z $startTime ];
-then
-  logging -w "Backup start time was not set!"
-  return 1
-fi
-
-zabbixHostName=$( ssh -i ${SSH_KEY} ${SSH_USER}@${vm_name} \
-    grep -Po "'(?<=^Hostname=).*'" \
-      /etc/zabbix/zabbix_agentd.conf)
-
-if [ -z $zabbixHostName ];
-then
-  logging -w "Could not recognize zabbix hostname!"
-  return 1
-fi
-
-timestamp=$(date +%s);
-duration=$(($timestamp - $startTime));
-Ret=0
-
-# Send the timestamp of the last successfull backup
-zabbix_sender --config /etc/zabbix/zabbix_agentd.conf \
-              --host "${zabbixHostName}" \
-              --key 'rabe.rabe-backup.run.success[]' \
-              --value "$timestamp" || Ret=$?
-
-# Send the duration of the last backup run in seconds
-zabbix_sender --config /etc/zabbix/zabbix_agentd.conf \
-              --host "${zabbixHostName}" \
-              --key 'rabe.rabe-backup.run.duration[]' \
-              --value "$duration" || Ret=$?
-
-return $Ret;
-}
-
-# trap keyboard interrupt (control-c)
-trap control_c SIGINT
-
-control_c()
-#
-# Description:  run if user hits control-c
-#
-# Parameter  :  none
-#
-# Output     :  logging
-#
-{
-logging -n "rabe-backup CTRL-C catched"
-exit 1
-}
-
-# Main -------------------------------------------------------------------------
-
-# Source logging: https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-. $SCRIPT_DIR/logging.lib
-
-# Configure rsync --bwlimit if backup is executed during the day
-BW_LIMIT_HOUR="`date +%H`"
-if [ "$BW_LIMIT_HOUR" -ge 7 -a "$BW_LIMIT_HOUR" -le 23 ];
-then
-    logging -i "Starting backup with limited bandwidth - 100 MBit/s"
-    RSYNC_OPTS="$RSYNC_OPTS --bwlimit=10240"
-fi
-
-mv ~/.ssh/known_hosts ~/.ssh/known_hosts.bkp
-
-logging -i "rabe-backup rsync backup of userdata starting."
-
-startTime="$(date +%s)";
-vm_name=".***REMOVED***";
-errors=0;
-ssh-keyscan $BACKUP_SRC_HOST >> ~/.ssh/known_hosts 2>/dev/null
-
-i=0
-until [ -z ${BACKUP_SRC_DIRS[i]} ];
-do
-
-syncdir="${BACKUP_SRC_HOST}:${BACKUP_SRC_DIRS[i]}"
-
-if [ ! -d ${BACKUP_DST_DIRS[i]} ];
-then
-    logging -e "Destination ${BACKUP_DST_DIRS[i]} is not a directory"
-    break;
-fi
-
-set -x
-rsync \
-  --progress \
-  --rsync-path="sudo /bin/rsync" \
-  --rsh="${RSH_CMD}" \
-  ${RSYNC_OPTS} \
-  ${syncdir} ${BACKUP_DST_DIRS[i]}
-set +x
-
-ret=$?
-if [ $ret -eq "0" ]
-then
-  logging -s "Sync of $syncdir to ${BACKUP_DST_DIRS[i]} successfull!"
-elif [ $ret -eq "23" ]
-then
-  logging -i "$syncdir does not exist."
-elif [ $ret -eq "12" ]
-then
-  logging -e "Permission denied on $syncdir."
-  let "errors++";
-elif [ $ret -eq "255" ]
-then
-  logging -e "Host $i.***REMOVED*** is not online or could not be resolved."
-  let "errors++";
-else
- logging -e "Unknown error ($ret) occured when trying to rsync $syncdir."
- let "errors++";
-fi
-
-i=$(( $i + 1 ))
-done
-
-if [ $errors -eq 0 ];
-then
-  if ! backup_success $startTime;
-  then
-      logging -e "backup_success: Could not send statuses for ${zabbixHostName} to zabbix"
+main(){
+  log -i "Rsync backup of userdata starting"
+  mv ~/.ssh/known_hosts ~/.ssh/known_hosts.bkp
+  ssh-keyscan "$BACKUP_SRC_HOST" >> ~/.ssh/known_hosts 2>/dev/null
+  local errs=0
+  local i=0
+  if [[ ! -d ${BACKUP_DST_DIRS[i]} ]]; then
+    log -e "Destination ${BACKUP_DST_DIRS[i]} is not a directory"
+    ((errs++))
   fi
-else
-  let "errors_vm_all++";
-  logging -w "$vm_name had problems during the backup job."
-fi
-
-mv ~/.ssh/known_hosts.bkp ~/.ssh/known_hosts
-logging -i "Script finished; $errors occured during the backup job."
-
-if [ $errors -gt 0 ];
-then
-  exit 1
-fi
+  until [[ -z ${BACKUP_SRC_DIRS[i]} ]]; do
+    if ! do_rsync "$BACKUP_SRC_HOST:${BACKUP_SRC_DIRS[i]}" "${BACKUP_DST_DIRS[i]}"; then
+      ((errs++))
+    fi
+    ((i++))
+  done
+  mv ~/.ssh/known_hosts.bkp ~/.ssh/known_hosts
+  log -i "Rsync backup user userdata finished with $errs problems"
+  exit "$errs"
+}
+main
+# vim: tabstop=2 shiftwidth=2 expandtab
