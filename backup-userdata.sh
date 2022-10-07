a936fad3b3773e8d4d3f76cfceb0611700864cc1
spameier <spameier@users.noreply.github.com>
2022-10-07 13:32:17 +0200

    (fix) replace while loop with for loop to get rid of unbound var

--- backup-userdata.sh
+++ backup-userdata.sh
@@ -22,12 +22,13 @@ main(){
   ssh-keyscan "$BACKUP_SRC_HOST" >> ~/.ssh/known_hosts 2>/dev/null
   local errs=0
   local i=0
-  if [[ ! -d ${BACKUP_DST_DIRS[i]} ]]; then
-    log -e "Destination ${BACKUP_DST_DIRS[i]} is not a directory"
-    ((errs++))
-  fi
-  until [[ -z ${BACKUP_SRC_DIRS[i]} ]]; do
-    if ! do_rsync "$BACKUP_SRC_HOST:${BACKUP_SRC_DIRS[i]}" "${BACKUP_DST_DIRS[i]}" "--no-relative"; then
+  for backup_src_dir in "${BACKUP_SRC_DIRS[@]}"; do
+    local backup_dst_dir="${BACKUP_DST_DIRS[i]}"
+    if [[ ! -d $backup_dst_dir ]]; then
+       log -e "Destination $backup_dst_dir is not a directory"
+       ((errs++))
+    fi
+    if ! do_rsync "$BACKUP_SRC_HOST:$backup_src_dir" "$backup_dst_dir" "--no-relative"; then
       ((errs++))
     fi
     ((i++))
